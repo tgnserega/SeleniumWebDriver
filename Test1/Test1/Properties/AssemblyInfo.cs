baa1e8aa47accdf828acc5a57f8428353fda905b
Sergey Pashkov <tgnserega@gmail.com>
2019-02-05 13:46:22 +0300

    Задание 1. Подготовить инфраструктуру

---
+++ Test1/Test1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Test1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Test1")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9a1dc76f-757f-4671-913f-0db34ada01d3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Test1/Test1/SeleniumTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumTests
+
+{
+    [TestFixture]
+    public class Test1
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void StartBrowsertest()
+        {
+            driver.Url = "https://yandex.ru/";
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            driver.Quit();
+        }
+    }
+}
