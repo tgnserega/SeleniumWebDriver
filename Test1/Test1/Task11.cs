a9c4292020e5ce36df1f8c61166f7d03c6dbf665
Sergey Pashkov <tgnserega@gmail.com>
2019-02-20 18:33:09 +0300

    Задание 11. Сделайте сценарий регистрации пользователя

---
+++ Test1/Test1/Task11.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumWebDriver
+{
+    public class Task11 : TestBase
+    {
+        [Test]
+
+        public void RegistrationTest()
+        {
+
+            driver.Navigate().GoToUrl(baseURL);
+
+            driver.FindElement(By.CssSelector("#box-account-login a")).Click();
+
+            driver.FindElement(By.CssSelector("[name='firstname']")).SendKeys("Ivanov");
+
+            driver.FindElement(By.CssSelector("[name='lastname']")).SendKeys("Ivan");
+
+            driver.FindElement(By.CssSelector("[name='address1']")).SendKeys("Russia");
+
+            driver.FindElement(By.CssSelector("[name='postcode']")).SendKeys("35005");
+
+            driver.FindElement(By.CssSelector("[name='city']")).SendKeys("Krasnodar");
+
+            IList<IWebElement> links = (IList<IWebElement>)((IJavaScriptExecutor)driver)
+
+            .ExecuteScript( @"arguments[0].value = 'US'; arguments[0].dispatchEvent(new Event('change')); ", driver.FindElement(By.CssSelector("[name='country_code']")));
+
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            var stringChars = new char[8];
+            var random = new Random();
+
+            for (int i = 0; i < stringChars.Length; i++)
+            {
+                stringChars[i] = chars[random.Next(chars.Length)];
+            }
+
+            var uniqueEmail = new String(stringChars) + "@bk.ru";
+
+            driver.FindElement(By.CssSelector("[name='email']")).SendKeys(uniqueEmail);
+
+            driver.FindElement(By.CssSelector("[name='phone']")).SendKeys("8800500");
+
+            driver.FindElement(By.CssSelector("[name='password']")).SendKeys("Password1");
+
+            driver.FindElement(By.CssSelector("[name='confirmed_password']")).SendKeys("Password1");
+
+            driver.FindElement(By.CssSelector("[name='create_account']")).Click();
+
+            driver.FindElement(By.XPath("//div[@id='box-account']//a[contains(.,'Logout')]")).Click();
+
+            driver.FindElement(By.CssSelector("[name='email']")).SendKeys(uniqueEmail);
+
+            driver.FindElement(By.CssSelector("[name='password']")).SendKeys("Password1");
+
+            driver.FindElement(By.CssSelector("form[name=login_form] button[name=login]")).Click();
+
+            driver.FindElement(By.XPath("//div[@id='box-account']//a[contains(.,'Logout')]")).Click();
+        }
+    }
+}
