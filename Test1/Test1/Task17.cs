737a055d32fa0efeb4788a7c799d3472dd7f9aeb
Sergey Pashkov <tgnserega@gmail.com>
2019-03-11 17:39:40 +0300

    Задание 17. Проверьте отсутствие сообщений в логе браузера

---
+++ Test1/Test1/Task17.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumWebDriver
+{
+    public class Task17 : TestBase
+    {
+        [Test]
+
+        public void LogsTest()
+        {
+            driver.Navigate().GoToUrl(baseURL + "admin/?app=catalog&doc=catalog&category_id=1/");
+
+            Login();
+
+            IList<IWebElement> listProduct = driver.FindElements(By.XPath(".//tr[.//a[contains(@href, 'product_id')]]"));
+
+            List<string> listProductLink = new List<string>();
+
+            for (int i = 0; i < listProduct.Count; i++)
+
+                listProductLink.Add(listProduct[i].FindElement(By.CssSelector("a[href*=product_id]")).GetAttribute("href"));
+
+            for (int i = 0; i < listProductLink.Count; i++)
+
+            {
+                driver.Url = listProductLink[i];
+
+                foreach (LogEntry l in driver.Manage().Logs.GetLog("browser"))
+                {
+                    Console.WriteLine(l);
+                }
+            }
+        }
+    }
+}
